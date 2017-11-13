997699b654e1312db28b2defdeadcb00f4bda1a5
joachimharms <joachimharms77@gmail.com>
2017-11-13 20:36:48 +0100

    Add StringBuilder object to elapse time for string operations - done

--- L001 StringBuilder Klasse/Program.cs
+++ L001 StringBuilder Klasse/Program.cs
@@ -11,19 +11,37 @@ namespace L001_StringBuilder_Klasse
     {
         static void Main(string[] args)
         {
-            // Strings sind unveränderlich und jede Stringmanipulation sorgt dafür, dass ein neues 
-            // String Objekt erschaffen wird. Bei vielen Operationen kostet dies viel Zeit: 
+            // Strings sind unveränderlich und jede Stringmanipulation sorgt dafür, dass ein neues
+            // String Objekt erschaffen wird. Bei vielen Operationen kostet dies viel Zeit:
             var watch = new Stopwatch();
             string text = "";
             watch.Start();
 
-            for (int i = 0; i < 50000; i++)
+            for (int i = 0; i < 150000; i++)
             {
-                text += "x"; 
+                text += "x";
             }
 
             watch.Stop();
             Console.WriteLine($"Zeit: {watch.ElapsedMilliseconds} ms");
+
+
+            // Dasselbe listing mit einem StringBuilder Objekt:
+            //var watch = new Stopwatch();
+            //string text = "";
+            //var sb = new StringBuilder(text);
+            //watch.Start();
+
+            //for (int i = 0; i < 150000; i++)
+            //{
+            //    sb.Append("x");
+            //}
+            //watch.Stop();
+            //Console.WriteLine($"Zeit: {watch.ElapsedMilliseconds} ms");
+
+            // Ergebnis Variante 1 reine String concatination = 2500 ms
+            // Ergebnis vom StringBuilder Objekt Operationen = 1 ms!!!
+
         }
     }
 }
